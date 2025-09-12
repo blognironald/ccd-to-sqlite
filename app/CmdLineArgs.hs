3dd37d18ca09303c85758032f95d6675b92b06eb
revrojas <ronaldo.rojas@gmail.com>
2025-09-12 15:06:57 +0900

    Accepts command line args

---
+++ app/CmdLineArgs.hs
@@ -0,0 +1,37 @@
+module CmdLineArgs where
+
+import Options.Applicative
+
+data Args = Args
+  { operation   :: String
+  , input       :: String
+  , output      :: String 
+  }
+
+-- Parser for arguments
+argsParser :: Parser Args
+argsParser = Args
+      <$> strOption
+          ( long "operation"
+         <> long "op"
+         <> metavar "OPERATION"
+         <> help "Type of parse and sql-ize operation" )
+      <*> strOption
+          ( long "input"
+         <> short 'i'
+         <> metavar "TEXT"
+         <> help "Path to input file" )
+      <*> strOption
+          ( long "output"
+         <> short 'o'
+         <> metavar "DB"
+         <> showDefault
+         <> value "out.db3"
+         <> help "Path to output sqlite db" )
+
+-- Program info
+opts :: ParserInfo Args
+opts = info (argsParser <**> helper)
+    ( fullDesc
+  <> progDesc "Convert File to Sql Table"
+  <> header "ccd-to-sqlite - a simple file to sqlite db processor" )
--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,14 @@
 module Main where
 
+import CmdLineArgs
+import Options.Applicative
+
+-- import System.Environment (withArgs)
+-- withArgs ["--op", "ccd", "-iccd.txt", "-occd.db3"] main
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+    args <- execParser opts
+    putStrLn $ "First argument: " ++ operation args
+    putStrLn $ "Second argument: " ++ input args
+    putStrLn $ "Third argument: " ++ output args
