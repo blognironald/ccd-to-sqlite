93f6665cea8813745827dbf47756b17f6ae5b5dd
revrojas <589827+blognironald@users.noreply.github.com>
2025-10-03 15:17:44 +0900

    Fix warnings

--- app/UnihanReadingsParser.hs
+++ app/UnihanReadingsParser.hs
@@ -1,19 +1,16 @@
-{-# LANGUAGE RecordWildCards #-}
-
 module UnihanReadingsParser where
 
-import Prelude 
+import Prelude
 import Data.Text as T
 import Data.Text.IO as T
 import Text.Megaparsec.Char
 import Text.Megaparsec
 import Data.Void (Void)
 import Data.Char
-import qualified Text.Megaparsec.Char.Lexer as L
 import Data.Maybe (catMaybes)
 import Numeric (readHex)
-import Control.Monad (void)
 import System.Exit (die)
+import Data.List as L ( find )
 
 type Parser = Parsec Void T.Text
 
@@ -47,9 +44,9 @@ pEmptyLine = do
     return ()
 
 codePointToChar :: Text -> Maybe Text
-codePointToChar codePoint = 
+codePointToChar codePoint =
     case T.stripPrefix (T.pack "U+") codePoint of
-        Just hexStr -> 
+        Just hexStr ->
             case readHex (T.unpack hexStr) of
                 [(value, "")] -> Just $ T.singleton (chr value)
                 _ -> Nothing
@@ -62,7 +59,7 @@ pDataLine = do
     fieldName <- pFieldName
     _ <- char '\t'
     fieldValue <- pFieldValue
-    
+
     if fieldName == T.pack "kDefinition"
         then case codePointToChar codePoint of
             Just character -> return $ Just UnihanEntry {
@@ -92,7 +89,8 @@ runUnihanParser input = do
         Left err -> return $ Left (errorBundlePretty err)
         Right rows -> return $ Right rows
 
-runner = do
+runSample :: IO ()
+runSample = do
     result <- runUnihanParser "others/Unihan_Readings.txt"
     case result of
         Left err -> die err
@@ -109,7 +107,7 @@ charToCodePoint :: Char -> Text
 charToCodePoint c = T.pack $ "U+" ++ toHexString (fromEnum c)
     where
         toHexString n = let hex = showHex n ""
-                        in Prelude.map toUpper $ Prelude.replicate (4 - Prelude.length hex) '0' ++ hex
+                        in Prelude.map toUpper' $ Prelude.replicate (4 - Prelude.length hex) '0' ++ hex
         showHex n s
             | n < 16 = hexDigit n : s
             | otherwise = showHex (n `div` 16) (hexDigit (n `mod` 16) : s)
@@ -117,16 +115,13 @@ charToCodePoint c = T.pack $ "U+" ++ toHexString (fromEnum c)
             | n < 10 = chr (48 + n)
             | n < 16 = chr (55 + n)
             | otherwise = error "Invalid hex digit"
-        toUpper c'
-            | c' >= 'a' && c' <= 'z' = chr (fromEnum c' - 32)
+        toUpper' c'
+            | isAsciiLower c' = chr (fromEnum c' - 32)
             | otherwise = c'
 
 findByCharacter :: Text -> [UnihanEntry] -> Maybe UnihanEntry
-findByCharacter t entries = 
-    listToMaybe $ Prelude.filter (\e -> uniCharacter e == t) entries
-    where
-        listToMaybe [] = Nothing
-        listToMaybe (x:_) = Just x
+findByCharacter t = L.find (\e -> uniCharacter e == t)
+
 
 findByDefinitionWord :: Text -> [UnihanEntry] -> [UnihanEntry]
 findByDefinitionWord searchTerm = Prelude.filter containsWord
@@ -135,15 +130,15 @@ findByDefinitionWord searchTerm = Prelude.filter containsWord
         containsWord entry = T.isInfixOf lowerSearchTerm (T.toLower $ uniDefinition entry)
 
 getAllCharacters :: [UnihanEntry] -> Text
-getAllCharacters  = T.concat . fmap uniCharacter 
+getAllCharacters  = T.concat . fmap uniCharacter
 
 getStatistics :: [UnihanEntry] -> (Int, Int, Int)
 getStatistics entries = (totalEntries, avgDefLength, maxDefLength)
     where
         totalEntries = Prelude.length entries
         defLengths = Prelude.map (T.length . uniDefinition) entries
-        avgDefLength = if totalEntries > 0 
-                       then Prelude.sum defLengths `div` totalEntries 
+        avgDefLength = if totalEntries > 0
+                       then Prelude.sum defLengths `div` totalEntries
                        else 0
         maxDefLength = if Prelude.null defLengths then 0 else Prelude.maximum defLengths
 
